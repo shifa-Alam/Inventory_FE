898de600eafef57cced3272f917683b51659d43d
Dev Shifa <shifa.alam@lucidtech.com.bd>
2026-06-20 23:21:42 +0600

    Restore local dev API URL in environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8000'
-  apiUrl: 'https://inventorybe-production.up.railway.app'
+  apiUrl: 'http://localhost:8000'
+  //apiUrl: 'https://inventorybe-production.up.railway.app'
 };
